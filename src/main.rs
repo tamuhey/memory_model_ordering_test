b59f123a30c9176b470115386752d6092f8f842c
Yohei Tamura <tamuhey@gmail.com>
2021-04-08 01:40:55 +0900

    seqcst example

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{thread, time::Duration};
 
 fn sleep() {
     let mut rng = thread_rng();
-    let t = rng.gen_range(100..1000);
+    let t = rng.gen_range(100..120);
     thread::sleep(Duration::from_nanos(t));
 }
 
@@ -13,24 +13,49 @@ fn main() {
     let ntry = 100_000;
     for _ in 0..ntry {
         let x = Arc::new(AtomicBool::new(false));
-        let lock = Arc::new(AtomicBool::new(false));
+        let y = Arc::new(AtomicBool::new(false));
+        let z = Arc::new(AtomicBool::new(false));
+
         let th1 = {
             let x = Arc::clone(&x);
-            let lock = Arc::clone(&lock);
             thread::spawn(move || {
+                sleep();
                 x.store(true, Ordering::Relaxed);
-                lock.store(true, Ordering::Relaxed);
             })
         };
         let th2 = {
-            let x = Arc::clone(&x);
-            let lock = Arc::clone(&lock);
+            let y = Arc::clone(&y);
             thread::spawn(move || {
-                while !lock.load(Ordering::Relaxed) {}
-                assert!(x.load(Ordering::Relaxed));
+                sleep();
+                y.store(true, Ordering::Relaxed);
+            })
+        };
+        let th3 = {
+            let x = Arc::clone(&x);
+            let y = Arc::clone(&y);
+            let z = Arc::clone(&z);
+            thread::spawn(move || {
+                while !x.load(Ordering::Relaxed) {}
+                if y.load(Ordering::Relaxed) {
+                    z.store(true, Ordering::Relaxed);
+                }
+            })
+        };
+        let th4 = {
+            let x = Arc::clone(&x);
+            let y = Arc::clone(&y);
+            let z = Arc::clone(&z);
+            thread::spawn(move || {
+                while !y.load(Ordering::Relaxed) {}
+                if x.load(Ordering::Relaxed) {
+                    z.store(true, Ordering::Relaxed);
+                }
             })
         };
         th1.join().unwrap();
         th2.join().unwrap();
+        th3.join().unwrap();
+        th4.join().unwrap();
+        assert!(z.load(Ordering::Relaxed));
     }
 }
