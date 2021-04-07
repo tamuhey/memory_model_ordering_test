92f3b357dce94b3b04990de3ada58478b65a9fa7
Yohei Tamura <tamuhey@gmail.com>
2021-04-08 01:28:36 +0900

    aqcrel order

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::{thread, time::Duration};
 
-static mut v: bool = true;
-
 fn sleep() {
     let mut rng = thread_rng();
     let t = rng.gen_range(100..1000);
@@ -13,42 +11,26 @@ fn sleep() {
 
 fn main() {
     let ntry = 100_000;
-    let mut ret = [0usize, 0usize];
     for _ in 0..ntry {
-        unsafe {
-            v = true;
-        }
         let x = Arc::new(AtomicBool::new(false));
-        let y = Arc::new(AtomicBool::new(false));
+        let lock = Arc::new(AtomicBool::new(false));
         let th1 = {
             let x = Arc::clone(&x);
-            let y = Arc::clone(&y);
+            let lock = Arc::clone(&lock);
             thread::spawn(move || {
-                sleep();
-                let r1 = y.load(Ordering::Relaxed);
-                x.store(r1, Ordering::Relaxed);
-                unsafe {
-                    v &= r1;
-                };
+                x.store(true, Ordering::Relaxed);
+                lock.store(true, Ordering::Relaxed);
             })
         };
         let th2 = {
             let x = Arc::clone(&x);
-            let y = Arc::clone(&y);
+            let lock = Arc::clone(&lock);
             thread::spawn(move || {
-                sleep();
-                let r2 = x.load(Ordering::Relaxed);
-                y.store(true, Ordering::Relaxed);
-                unsafe {
-                    v &= r2;
-                };
+                while !lock.load(Ordering::Relaxed) {}
+                assert!(x.load(Ordering::Relaxed));
             })
         };
         th1.join().unwrap();
         th2.join().unwrap();
-        unsafe {
-            ret[v as usize] += 1;
-        }
     }
-    println!("{:?}", ret);
 }
